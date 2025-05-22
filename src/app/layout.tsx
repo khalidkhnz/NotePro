e305848477b8dadb0c4026a3cdbe0b4ea5bc0ed0
khalidkhnz <eternalkhalidkhnz@gmail.com>
2025-05-22 17:21:38 +0530

    Update favicon and change default theme to dark in layout component
    - Replaced the favicon with a new version for improved branding.
    - Changed the default theme setting in the layout component from "system" to "dark" for a more consistent user experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <TRPCReactProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
